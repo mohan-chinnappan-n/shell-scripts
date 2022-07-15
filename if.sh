b0278a23b4490b381e92616424b8d01d149334df
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2022-07-15 09:39:24 -0400

    init

--- if.sh
+++ if.sh
@@ -1,11 +1,11 @@
 B_NAME=$1
-if [ "${B_NAME}" == "devsubbranch1.1" ]; then
+if [ "${B_NAME}" == "main.1" ]; then
 	echo "$1"
 # if you put elif like this:
 # elif [ "${B_NAME}" == "developr2" ] 
 # you will get syntax error:
 #  syntax error near unexpected token `else' 
-elif [ "${B_NAME}" == "developr2" ]; then
+elif [ "${B_NAME}" == "release" ]; then
 	echo "$1"
 else 
 	echo "unknown branch"
