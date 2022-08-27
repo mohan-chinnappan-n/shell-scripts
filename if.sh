d74c5333c7d44f3449585a0597f7b1edff006a24
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2022-08-27 16:54:31 -0400

    cleanup

--- if.sh
+++ if.sh
@@ -1,13 +1,25 @@
-B_NAME=$1
-if [ "${B_NAME}" == "main.1" ]; then
-	echo "$1"
-# if you put elif like this:
-# elif [ "${B_NAME}" == "developr2" ] 
-# you will get syntax error:
-#  syntax error near unexpected token `else' 
-elif [ "${B_NAME}" == "release" ]; then
-	echo "$1"
-else 
-	echo "unknown branch"
+#!/bin/bash
+
+
+# format
+# -----------
+#if TEST-COMMAND then
+#  STATEMENTS
+# elif TEST-COMMAND then 
+#  STATEMENTS
+#fi
+
+
+
+#if the variable is not set in $1 use the default "apple"
+fruit="${1:-apple}"
+
+echo $fruit
+if [ $fruit == "apple" ]; then 
+	echo "An $fruit day keeps the doctor away!"
+elif [ $fruit == "mango" ]; then 
+	echo "$fruit makes happy!"
+
 
 fi
+
