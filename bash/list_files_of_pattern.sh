d1d60e33f8884641362f06cce13a3f882a3f3f28
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2023-02-17 15:51:38 -0500

    Update list_files_of_pattern.sh
    cleanup

--- bash/list_files_of_pattern.sh
+++ bash/list_files_of_pattern.sh
@@ -2,5 +2,5 @@
 ## go to top folder and run the following command
 find . -name '* 2.*' >> /tmp/dup_files.txt
 
-# to delete these files, uncomment the next lines
+# to delete these files, uncomment the next line
 # cat /tmp/dup_files.txt | sed 's/\(.*\)/"\1"/g' | xargs rm -f 
