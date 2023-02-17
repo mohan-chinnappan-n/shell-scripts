75c26dd2df68c532dfe369223a495d92f0c62597
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2023-02-17 15:33:49 -0500

    Update list_files_of_pattern.sh

--- bash/list_files_of_pattern.sh
+++ bash/list_files_of_pattern.sh
@@ -2,4 +2,4 @@
 # go to top folder
 find . -name '* 2.*' >> /tmp/dup_files.txt
 # delete these files, uncomment the next lines
-# xargs rm -rf /tmp/dup_files.txt
+# xargs rm -rf < /tmp/dup_files.txt
