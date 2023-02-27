75af97daf4bc01b958753ce255dd1f0b18e9db66
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2023-02-27 09:12:04 -0500

    Update list_files_of_pattern.sh
    updated for '* copy.*'

--- bash/list_files_of_pattern.sh
+++ bash/list_files_of_pattern.sh
@@ -1,6 +1,8 @@
-## list files of pattern  '* 2.*'
+## list files of pattern  '* 2.*' and '* copy.*' 
 ## go to top folder and run the following command
-find . -name '* 2.*' > /tmp/dup_files.txt
+find . -name '* 2.*' > /tmp/2_dup_files.txt
+find . -name '* copy.*' > /tmp/copy_dup_files.txt
 
 # to delete these files, uncomment the next line
-# cat /tmp/dup_files.txt | sed 's/\(.*\)/"\1"/g' | xargs rm -f 
+# cat 2_dup_files.txt| sed 's/\(.*\)/"\1"/g' | xargs rm -f 
+# cat /tmp/copy_dup_files.txt | sed 's/\(.*\)/"\1"/g' | xargs rm -f 
