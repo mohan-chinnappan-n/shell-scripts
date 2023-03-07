6449dcbb16596497f30bc42716a94d31ba8206af
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2023-03-07 09:48:00 -0500

    cleanup

--- bash/list_files_of_pattern.sh
+++ bash/list_files_of_pattern.sh
@@ -3,7 +3,7 @@
 
 find . -name '* 2.*'  2>&1 | tee /tmp/2_dup_files.txt
 
-find . -name '* copy.*'  2>&1 | tee /tmp/2_dup_files.txt
+find . -name '* copy.*'  2>&1 | tee /tmp/copy_dup_files.txt
 
 
 # to delete these files, uncomment the next line
